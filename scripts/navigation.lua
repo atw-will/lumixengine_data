726912d0bb4f7a6e970bd8873df3a0f4b238e83c
Will Robinson <chalicier@hotmail.com>
2018-06-16 04:59:01 +0100

    Navigation pathing now working

--- scripts/navigation.lua
+++ scripts/navigation.lua
@@ -1,14 +1,18 @@
 Gui.enableCursor(Gui.instance, true)
-Navigation.load(g_scene_navigation, "universes/navigation.nav")
+Navigation.load(g_scene_navigation, "universes/subverted.nav")
 speed = 5
+stopdist = 0.2
 
-function update()
-	if Gui.isMouseClicked(Gui.instance, 0) then
-		local x = Gui.getMouseX(Gui.instance)
-		local y = Gui.getMouseY(Gui.instance)
-		local is_hit, pos = Renderer.castCameraRay(g_scene_renderer, "main", x, y)
-		if is_hit then
-			Navigation.navigate(g_scene_navigation, this, pos, speed)
-		end
-	end
+
+function onInputEvent(event)
+if event.type == Engine.INPUT_EVENT_BUTTON then
+    if event.device.type == Engine.INPUT_DEVICE_MOUSE then
+        if event.key_id == 1 then
+            local is_hit, pos = Renderer.castCameraRay(g_scene_renderer, "main", event.x_abs, event.y_abs)
+            if is_hit then
+                Navigation.navigate(g_scene_navigation, this, pos, speed, stopdist)
+            end
+        end
+    end
+end
 end
