7b0e7476b9dfb437b5f5331ccd69e807b6267be9
Will Robinson <chalicier@hotmail.com>
2018-07-21 13:47:52 +0100

    Further improvements to sensorium

--- scripts/sensorium.lua
+++ scripts/sensorium.lua
@@ -2,6 +2,8 @@
 -- Draws raycasts to entities within a given sensory radius, and maintains a list of those entities 
 -- This version works in 360, the final version will operate based on head position/rotation and awareness arc
 
+local maf = require("scripts\\maf\\maf")
+
 -- position from which raycasts are made
 viewpoint = -1
 Editor.setPropertyType("viewpoint", Editor.ENTITY_PROPERTY) 
@@ -19,58 +21,111 @@ local visualSensed = {}
 local audioSensed = {}
 
 function init()
-    
+    clear()
+end
+
+function clear()
+	count = #visualRangeObjects
+	for i=0, count do visualRangeObjects[i]=nil end
+
+	count = #visualSensed
+	for i=0, count do visualSensed[i]=nil end
+
+	count = #audioSensed
+	for i=0, count do audioSensed[i]=nil end
+end
+
+function get_vector(arrayVersion)
+	return maf.vector(arrayVersion[1],arrayVersion[2],arrayVersion[3])
+end
+
+function get_Vec3(vectorVersion)
+	return {vectorVersion.x, vectorVersion.y, vectorVersion.z}
 end
 
 function get_range()
 
-    visualSensed = {}
-	visualRangeObjects = {}
-	audioSensed = {}
-
-	currPos = Engine.getEntityPosition(this)
-
+	currPos = get_vector(Engine.getEntityPosition(g_universe,  this))
 	-- check all objects stemming from map_root and char_root 
 	
 	map_root = Engine.findByName(g_universe,-1,"map_root")
+	if map_root == -1 then Engine.logInfo("map_root not found") end
 	child = Engine.getFirstChild(g_universe, map_root)
-	while child ~= -1 do
-		newPos = Engine.getEntityPosition(child)
-		between = currPos - newPos
-		dist = sqrt(between.x * between.x + between.y * between.y + between.z * between.z )
-		if dist < visualRange then table.insert(visualRangeObjects, {entity=child, distance=dist, pos=newPos}) end
-		if dist < audioRange then table.insert(audioSensed, {entity=child, distance=dist, pos=newPos}) end
+	if child == -1 or child == nil then Engine.logInfo("No Children Found For map_root") end
+	
+	while child ~= nil and child ~= -1 do
+		newPos = get_vector(Engine.getEntityPosition(g_universe, child))
+		between = currPos - newPos;
+		dist = between:length()
+
+		if dist < visualRange then
+			table.insert(visualRangeObjects, {entity=child, distance=dist, pos=newPos})
+		end
+
+		if dist < audioRange then
+			table.insert(audioSensed, {entity=child, distance=dist, pos=newPos})
+		end
+		
+		child = Engine.getNextSibling(g_universe, child)
 	end
 			
 	char_root = Engine.findByName(g_universe,-1,"char_root")
+	if char_root == -1 then Engine.logInfo("char_root not found") end
 	child = Engine.getFirstChild(g_universe, char_root)
-	while child ~= -1 do
-		newPos = Engine.getEntityPosition(child)
-		between = currPos - newPos
-		dist = sqrt(between.x * between.x + between.y * between.y + between.z * between.z )
-		if dist < visualRange then table.insert(visualRangeObjects, {entity=child, distance=dist, pos=newPos}) end
-		if dist < audioRange then table.insert(audioSensed, {entity=child, distance=dist, pos=newPos}) end
+	if child == -1 or child == nil then Engine.logInfo("No Children Found For char_root") end
+
+	while child ~= nil and child ~= -1 do
+		newPos = get_vector(Engine.getEntityPosition(g_universe, child))
+		between = currPos - newPos;
+		dist = between:length()
+		
+		if dist < visualRange then
+			store = {entity=child, distance=dist, pos=newPos}
+			table.insert(visualRangeObjects, store)
+		end
+
+		if dist < audioRange then
+			store = {entity=child, distance=dist, pos=newPos}
+			table.insert(audioSensed, store)
+		end
+
+		child = Engine.getNextSibling(g_universe, child)
 	end
+
 end
 
 function check_LOS()
-	
-	if viewpoint==-1 then
-		startPos = Engine.getEntityPosition(viewpoint)
+	local startPos = maf.vector(0,0,0)
+
+	if viewpoint~=-1 then
+		startPos = get_vector(Engine.getEntityPosition(g_universe,  viewpoint))
+		Engine.logInfo("Viewpoint Entity:" .. viewpoint)
 	else
-		startPos = Engine.getEntityPosition(this)
+		startPos = get_vector(Engine.getEntityPosition(g_universe, this))
+		Engine.logInfo("No Viewpoint Entity:" .. this)
 	end
 
-	for target in visualRangeObjects do
-		dirVec = target[pos] - startPos
-		is_hit, hit_entity, hit_position = Physics.raycast(g_scene_physics, startPos, dirVec)
-		if is_hit and hit_entity == target[entity] then 
-            visualSensed.insert(target)
+	for index,target in ipairs(visualRangeObjects) do
+		position = target["pos"]
+		entity = target["entity"]
+		dirVec = position - startPos
+		dirVec:normalize()
+		Engine.logInfo("Entity:" .. entity .. " Position:".. position.x .. "," .. position.y .. "," .. position.z .. " Direction:" .. dirVec.x .. "," .. dirVec.y .. "," .. dirVec.z)
+		
+		is_hit, hit_entity, hit_position = Physics.raycast(g_scene_physics, get_Vec3(startPos), get_Vec3(dirVec))
+		if is_hit then
+			Engine.logInfo("Hit Entity " .. hit_entity)
+			if hit_entity == entity then 
+				Engine.logInfo("Hit the right thing! Wow!")
+				visualSensed.insert(target)
+			end
         end
 	end
 end
 
 function observe()
+    Engine.logInfo("observing...")
+	clear()
 	get_range()
 	check_LOS()
 end
